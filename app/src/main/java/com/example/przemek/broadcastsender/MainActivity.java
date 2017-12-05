26c6e960436e3fff9a790bd586ede316d85f4ac3
Przemek Latoch <przemek.latoch@gmail.com>
2017-12-05 18:26:59 +0100

    Broadcast sender working

---
+++ app/src/main/java/com/example/przemek/broadcastsender/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.przemek.broadcastsender;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et_message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et_message = (EditText) findViewById(R.id.et_message);
+    }
+
+    public void sendOutBroadcast(View view) {
+        Intent intent = new Intent();
+        intent.setAction("com.example.przemek.broadcastsender.MY_INTENT");
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        String message = et_message.getText().toString();
+        intent.putExtra("message", message);
+        sendBroadcast(intent, "com.example.przemek.broadcastsender.PERMISSION");
+    }
+}
